8cb7ef543e61081b0fa378644dfe913f9666e438
Enitan <enitanolawale0@gmail.com>
2023-10-02 04:20:44 +0100

    experimented with the search component

--- src/components/search.js
+++ src/components/search.js
@@ -4,13 +4,13 @@ import { useList } from './stateProvider'
 
 export const Search = ({onSubmit}) => {
 
-    const { searchTerm, setSearchTerm, setWeather, setLoading } = useList()
+    const { searchTerm, setSearchTerm } = useList()
 
     const onSearchchange = (e) => setSearchTerm(e.target.value)
 
     return <div>
-        <form onSubmit={onSubmit} className="w-1/2 h-[3rem] border">
-            <input type="text" value={searchTerm} onChange={onSearchchange} className="w-[90%] h-[90%] focus:outline-none bg-transparent" />
+        <form onSubmit={onSubmit} className="w-1/2 h-[3rem] border pl-3">
+            <input type="text" value={searchTerm} placeholder='search for a city' onChange={onSearchchange} className="w-[90%] h-[90%] focus:outline-none bg-transparent" />
 
             <button type="submit" className='h-full w-[10%] text-white'>
                 <FaSearch className='mx-auto' />
