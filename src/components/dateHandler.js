a7a87531bd5c7b2c25476b704a56c8c916572fe9
Enitan <enitanolawale0@gmail.com>
2023-10-03 04:56:20 +0100

    changed the logic and styling of the date component

--- src/components/dateHandler.js
+++ src/components/dateHandler.js
@@ -6,11 +6,9 @@ export const DateComp = () => {
     
     const { weather, setHour } = useList()
 
-    useEffect(() => {hourHandler(weather.location.localtime)}, [])
+    useEffect(() => {hourHandler(dateHandler(weather.location.localtime)[1])}, [])
 
-    const hourHandler = (date) => {
-        const dateObj = new Date(date)
-        const hour = dateObj.getHours()
+    const hourHandler = (hour) => {
         setHour(hour)
     }
 
@@ -24,12 +22,13 @@ export const DateComp = () => {
         const monthArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
         const hour = dateObj.getHours()
         const minute = dateObj.getMinutes()
+        const simplifiedTime = `${hour < 10 ? `0${hour}` : hour}:${minute < 10 ? `0${minute}` : minute} ${daysArr[dayOfWeek]}, ${day} ${monthArr[month]} ${year}`
     
-        return `${daysArr[dayOfWeek]}, ${day} ${monthArr[month]} ${year} | Local time: ${hour < 10 ? `0${hour}` : hour}:${minute < 10 ? `0${minute}` : minute}`
+        return [simplifiedTime, hour]
     }
 
     return <p className='my-5'>
-        {dateHandler(weather.location.localtime)}
+        {dateHandler(weather.location.localtime)[0]}
     </p>
 }
 
