3b7a03ee12c57bd745111bc63a5a1a6577fb2f16
Enitan <enitanolawale0@gmail.com>
2023-10-05 03:17:37 +0100

    some adjustments for mobile UI

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -40,23 +40,23 @@ export const NavBar = () => {
         clickFetch()
     }
 
-    return <nav className="md:text-2xl text-sm flex flex-row md:justify-center relative py-6">
-        <span onClick={() => searchWeather('tokyo')} className={`md:mr-[3rem] min-[300px]:mx-[1rem] mx-2 ${activeCategory == 'tokyo' ? 'text-[#004346]' : 'hover:text-[#004346]'} cursor-pointer`}>
+    return <nav className="md:text-2xl text-sm flex flex-row min-[100px]:max-md:bg-[#004346] md:justify-center relative py-6">
+        <span onClick={() => searchWeather('tokyo')} className={`md:mr-[3rem] min-[300px]:mx-[1rem] mx-2 ${activeCategory == 'tokyo' ? 'md:text-[#004346] text-[rgb(62,130,247)]' : 'md:hover:text-[#004346]'} cursor-pointer`}>
             Tokyo
         </span>
-        <span onClick={() => searchWeather('london')} className={`md:mr-[3rem] min-[300px]:mr-[1rem] hidden lg:flex ${activeCategory == 'london' ? 'text-[#004346]' : 'hover:text-[#004346]'} cursor-pointer`}>
+        <span onClick={() => searchWeather('london')} className={`mr-[3rem] hidden lg:flex ${activeCategory == 'london' ? 'text-[#004346]' : 'hover:text-[#004346]'} cursor-pointer`}>
             London
         </span>
-        <span onClick={() => searchWeather('paris')} className={`md:mr-[3rem] min-[300px]:mr-[1rem] mr-2 ${activeCategory == 'paris' ? 'text-[#004346]' : 'hover:text-[#004346]'} cursor-pointer`}>
+        <span onClick={() => searchWeather('paris')} className={`md:mr-[3rem] min-[300px]:mr-[1rem] mr-2 ${activeCategory == 'paris' ? 'md:text-[#004346] text-[rgb(62,130,247)]' : 'md:hover:text-[#004346]'} cursor-pointer`}>
             Paris
         </span>
-        <span onClick={() => searchWeather('sydney')} className={`md:mr-[3rem] min-[300px]:mr-[1rem] mr-2 ${activeCategory == 'sydney' ? 'text-[#004346]' : 'hover:text-[#004346]'} cursor-pointer`}>
+        <span onClick={() => searchWeather('sydney')} className={`md:mr-[3rem] min-[300px]:mr-[1rem] mr-2 ${activeCategory == 'sydney' ? 'md:text-[#004346] text-[rgb(62,130,247)]' : 'md:hover:text-[#004346]'} cursor-pointer`}>
             Sydney
         </span>
-        <span onClick={() => searchWeather('istanbul')} className={`md:mr-[3rem] ${activeCategory == 'istanbul' ? 'text-[#004346]' : 'hover:text-[#004346]'} cursor-pointer`}>
+        <span onClick={() => searchWeather('istanbul')} className={`md:mr-[3rem] ${activeCategory == 'istanbul' ? 'md:text-[#004346] text-[rgb(62,130,247)]' : 'hover:text-[#004346]'} cursor-pointer`}>
             Istanbul
         </span>
-        <span className="absolute right-1 min-[300px]:right-8 top-6 md:right-[5%] md:top-7">
+        <span className="absolute right-1 min-[300px]:right-3 top-6 md:right-[5%] md:top-7">
             <ToggleButton value = {fahrenheit} onToggle={() => setFahrenheit(!fahrenheit)} activeLabel='ºC' inactiveLabel='ºF'       colors={{
                 activeThumb: {
                     base: 'rgb(62,130,247)',
--- src/components/search.js
+++ src/components/search.js
@@ -8,13 +8,12 @@ export const Search = ({onSubmit}) => {
 
     const onSearchchange = (e) => setSearchTerm(e.target.value)
 
-    return <div>
-        <form onSubmit={onSubmit} className="lg:w-1/2 w-[70%] h-[3rem] border border-white pl-3 pr-3 lg:pr-0">
-            <input type="text" value={searchTerm} placeholder='search for a city' onChange={onSearchchange} className="w-[90%] h-[90%] focus:outline-none bg-transparent" />
+    return <form onSubmit={onSubmit} className="lg:w-1/2 w-[70%] h-[3rem] border border-white pl-3 pr-3 lg:pr-0 min-[100px]:max-md:mt-4">
+        <input type="text" value={searchTerm} placeholder='search for a city' onChange={onSearchchange} className="w-[90%] h-[90%] focus:outline-none bg-transparent" />
 
-            <button type="submit" className='h-full w-[10%]'>
-                <FaSearch className='mx-auto' />
-            </button>
-        </form>
-    </div>
+        <button type="submit" className='h-full w-[10%]'>
+            <FaSearch className='mx-auto' />
+        </button>
+    </form>
+    
 }
